902873fdf06ce90f5dc2402ff65a04999a52b2f3
qvenge <coovenbm@gmail.com>
2020-08-14 16:26:25 +0700

    add gender-selector block to fsd.blocks

---
+++ src/fsd.blocks/guest-list/guest-list.js
@@ -0,0 +1,66 @@
+function GuestList(elem) {
+    this.elem = elem;
+}
+
+
+var proto = GuestList.prototype;
+
+proto.id = 'guest-list';
+
+
+proto.init = function() {
+    var self = this;
+    var reset = this.elem.querySelector('.' + this.id + '__reset-btn');
+    var apply = this.elem.querySelector('.' + this.id + '__apply-btn');
+    var dropdown = this.elem.querySelector('.' + this.id + '__dropdown').bemEntities['dropdown'];
+    var list = this.elem.querySelector('.' + this.id + '__content').bemEntities['quantitative-list'];
+
+    this.elem.addEventListener('qlstatechanged', function() {
+        var listState = list.getState();
+        var guestQuantity = 0;
+        var babiesQuantity = 0;
+        var output = [];
+
+        listState.forEach(function(st) {
+            st.name === 'babies' ? (babiesQuantity += st.value) : (guestQuantity += st.value);
+        });
+
+        guestQuantity && (output.push(self.getOutput(guestQuantity, ['гость', 'гостя', 'гостей'])));
+        babiesQuantity && (output.push(self.getOutput(babiesQuantity, ['младенец', 'младенца', 'младенцов'])));
+
+        dropdown.output = output.length ? output.join(', ') : dropdown.defaultOutput;
+    });
+
+    reset.addEventListener('click', function() {
+        list.reset();
+        dropdown.close();
+    });
+
+    apply.addEventListener('click', function() {
+        dropdown.close();
+    });
+};
+
+
+proto.getOutput = function(number, words) {
+    var num = number % 100;
+    if (num > 19) {
+        num = num % 10;
+    }
+    switch (num) {
+        case 1: {
+            return number + ' ' + words[0];
+        }
+        case 2: case 3: case 4: {
+            return number + ' ' + words[1];
+        }
+        default: {
+            return number + ' ' + words[2];
+        }
+    }
+};
+
+
+if (module) {
+    module.exports = GuestList;
+}
