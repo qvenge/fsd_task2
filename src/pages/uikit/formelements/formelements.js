7888709359c1f09ef58eb0bf6ee6dbbcde8074c7
qvenge <coovenbm@gmail.com>
2020-08-14 16:31:10 +0700

    add formelements and colorsandtype pages

---
+++ src/pages/uikit/formelements/formelements.js
@@ -0,0 +1,12 @@
+'use strict';
+require('./style.scss');
+
+// const html = require('./some.pug');
+
+// const uiForms = document.querySelectorAll('.ui-form-element');
+// uiForms[uiForms.length - 1].innerHTML = html;
+
+// const content = document.createElement('DIV');
+// content.classList.add('ui-form-element__content');
+// content.innerHTML = html;
+// uiForms[uiForms.length - 2].append(content);
