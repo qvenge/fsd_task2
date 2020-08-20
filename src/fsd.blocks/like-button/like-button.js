0d5a0df0a146b57523bea06304434775274ef96c
qvenge <coovenbm@gmail.com>
2020-08-20 16:54:03 +0700

    add like-button block to fsd.blocks

---
+++ src/fsd.blocks/like-button/like-button.js
@@ -0,0 +1,58 @@
+function LikeButton(elem) {
+    this.elem = elem;
+    this._number = elem.getElementsByClassName(this.id + '__number')[0];
+
+    this.init();
+}
+
+LikeButton.prototype = {
+    id: 'like-button',
+
+    init: function() {
+        var self = this;
+
+        this.elem.addEventListener('click', function(event) {
+            self.toggle();
+        });
+    },
+
+    like: function() {
+        if (this.isLiked) {
+            return false;
+        }
+
+        this.elem.classList.add(this.id + '_liked');
+        this._number.textContent = Number(this._number.textContent) + 1;
+
+        return true;
+    },
+
+    dislike: function() {
+        if (!this.isLiked) {
+            return false;
+        }
+
+        this.elem.classList.remove(this.id + '_liked');
+        this._number.textContent = Number(this._number.textContent) - 1;
+
+        return true;
+    },
+
+    toggle: function() {
+        this.like() || this.dislike(); 
+    }
+};
+
+Object.defineProperties(LikeButton.prototype, {
+    isLiked: {
+        get: function() {
+            return this.elem.classList.contains(this.id + '_liked');
+        }
+    },
+})
+
+LikeButton.prototype.constructor = LikeButton;
+
+if (module) {
+    module.exports = LikeButton;
+}
